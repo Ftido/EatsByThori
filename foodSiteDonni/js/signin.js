d117789728bd3d3fee398eb88a8967fd99c6c106
Thato <thatolethedi7@gmail.com>
2023-06-15 14:59:36 +0200

    -changes

--- foodSiteDonni/js/signin.js
+++ foodSiteDonni/js/signin.js
@@ -1,18 +1,19 @@
 // Sign In js
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting
   
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    console.log("works");
     
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    // Perform your login validation here
-    
-  
     // Redirect to index.html if the login is successful
-    if (username === "your_username" && password === "your_password") {
-      window.location.href = "index.html";
-    } else {
+    if (username === "your_username" && password === "your_password") 
+    {
+      window.location.href = 'admin_page.php';
+    } 
+    else 
+    {
       alert("Invalid username or password. Please try again.");
     }
   });
